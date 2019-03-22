d23e9e80b6989f1f6b39f58a252a9e73e13898cc
LiyliLee <liyli@ucm.es>
2019-03-22 12:59:35 +0100

    Corregí el disparo contra el escudo del enemigo

--- Proyecto1/Assets/Scripts/ShootDamage.cs
+++ Proyecto1/Assets/Scripts/ShootDamage.cs
@@ -6,8 +6,15 @@ public class ShootDamage : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(collision.gameObject); // Es un código simple para BlackBullet, destruirá todo lo que se va a colisionar. Entonces hay que hace que la máscara 
-        Destroy(this.gameObject); // de colisión de Blackbullet solo se puede colisionar con breakWall y Enemy.
-        // BreakWall es un simple sprite con box collider2d, no tiene scripts.
+        if (collision.gameObject.tag == "EnemyShield")
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Destroy(collision.gameObject); // Es un código simple para BlackBullet, destruirá todo lo que se va a colisionar. Entonces hay que hace que la máscara 
+            Destroy(this.gameObject); // de colisión de Blackbullet solo se puede colisionar con breakWall y Enemy.
+                                      // BreakWall es un simple sprite con box collider2d, no tiene scripts.
+        }
     }
 }
