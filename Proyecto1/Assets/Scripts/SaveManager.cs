cda092be0a20d496ae6b69d8ad57ec4b2cb7f61b
Drathijin <rsulbara@ucm.es>
2019-03-25 19:52:21 +0100

    Añadiendo las primeras funcionalidades de escritura de archivos

--- Proyecto1/Assets/Scripts/SaveManager.cs
+++ Proyecto1/Assets/Scripts/SaveManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,36 +14,45 @@ public class SaveManager : MonoBehaviour {
         public int act; //nivel actual
     }
 
-    string dir = "saves.txt";
-    
+    string dir = "./saves.txt";
+
+    private void Start()
+    {
+        SaveGame(LoadGame(12));
+    }
     //Carga de un txt el estado del último guardado, en caso de que no haya dicho txt devuelve nivel 1 y el resto bloqueados
     public LevelsList LoadGame(int levels) 
     {
-        LevelsList ActualList = new LevelsList { levels = new LevelState[levels], act = 0};
-        for (int i = 0; i < ActualList.levels.Length; i++) ActualList.levels[i] = 0;
-        return ActualList;
+
+        LevelsList actualList = new LevelsList { levels = new LevelState[levels], act = 0};
+        for (int i = 0; i < actualList.levels.Length; i++) actualList.levels[i] = 0;
+        return actualList;
     }
 
     //Guarda en un txt el estado del juego
-    public void SaveGame()
+    public void SaveGame(LevelsList actualList)
     {
-
+        StreamWriter writer = new StreamWriter(dir);
+        writer.WriteLine(actualList.act);
+        foreach(LevelState level in actualList.levels)
+        writer.WriteLine(level);
+        writer.Close();
     }
 
     //Devuelve el último nivel sin bloquear
-    public int ActualLevel(ref LevelsList ActualList)
+    public int ActualLevel(ref LevelsList actualList)
     {
         int i = 0;
-        while (ActualList.levels[i] != LevelState.Locked) i++;
-        if (i<ActualList.act) ActualList.act = i;
+        while (actualList.levels[i] != LevelState.Locked) i++;
+        if (i<actualList.act) actualList.act = i;
         return i;
     }
 
     //Guarda una victoria o una derrota (dependiendo del bool) en la lista
-    public void SaveLevel(bool win, ref LevelsList ActualList)
+    public void SaveLevel(bool win, ref LevelsList actualList)
     {
-        if (win) ActualList.levels[ActualList.act] = LevelState.Win;
-        else ActualList.levels[ActualList.act] = LevelState.Lost;
-        ActualList.act++;
+        if (win) actualList.levels[actualList.act] = LevelState.Win;
+        else actualList.levels[actualList.act] = LevelState.Lost;
+        actualList.act++;
     }
 }
