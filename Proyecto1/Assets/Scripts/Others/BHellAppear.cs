b8c148d41670a369442050f9d8c7ece756bcd7b2
LiyliLee <liyli@ucm.es>
2019-04-24 10:51:55 +0200

    Crear la escena de Level9

---
+++ Proyecto1/Assets/Scripts/Others/BHellAppear.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BHellAppear : MonoBehaviour {
+
+    public GameObject[] bulletHells;
+    public float TimeAppear, TimeDisappear;
+    int n = 0, l = 0;
+	
+	// Update is called once per frame
+	void Start () {
+        InvokeRepeating("Appear", TimeAppear,TimeAppear );
+        
+        InvokeRepeating("Disappear", TimeDisappear,TimeDisappear );
+        
+    }
+    void Appear()
+    {
+        bulletHells[n].SetActive(true);
+        if (n < bulletHells.Length - 1)
+        {
+            n++;
+        }
+        else n = 0;
+
+    }
+    void Disappear()
+    {
+        
+        bulletHells[l].SetActive(false);
+        
+
+        if (l < bulletHells.Length - 1)
+        {
+            l++;
+        }
+        else l = 0;
+    }
+}
