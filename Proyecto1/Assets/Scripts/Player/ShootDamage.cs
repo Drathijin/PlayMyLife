88096d77d3c010b8999b29e0a2e07a3bf501b159
NicoPast <47886480+NicoPast@users.noreply.github.com>
2019-05-04 00:01:05 +0200

    Añadidos ultimos sprites y mejorados varios niveles

--- Proyecto1/Assets/Scripts/Player/ShootDamage.cs
+++ Proyecto1/Assets/Scripts/Player/ShootDamage.cs
@@ -9,12 +9,22 @@ public class ShootDamage : MonoBehaviour {
         Destroy(this.gameObject);
         if (collision.gameObject.tag != "NotDestructible")
         {
-            Destroy(collision.gameObject); 
-            // Es un código simple para PlayerBullet, destruya todo lo que se va a colisionar. Entonces hay que hace que la máscara 
-            // de colisión de Playerbullet solo pueda colisionar con breakWall y Enemy.
-            // BreakWall es un simple sprite con box collider2d, no tiene scripts.
-            GameManager.instance.KillEnemy();
+            if (collision.gameObject.tag == "BreakWall")
+            {
+                Animator wall = collision.gameObject.GetComponent<Animator>();
+                if (wall != null)
+                {
+                    wall.SetTrigger("Open");
+                }
+            }
+            else
+            {
+                Destroy(collision.gameObject);
+                // Es un código simple para PlayerBullet, destruya todo lo que se va a colisionar. Entonces hay que hace que la máscara 
+                // de colisión de Playerbullet solo pueda colisionar con breakWall y Enemy.
+                // BreakWall es un simple sprite con box collider2d, no tiene scripts.
+                GameManager.instance.KillEnemy();
+            }
         }
-
     }
 }
